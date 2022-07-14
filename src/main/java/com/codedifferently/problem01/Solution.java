f6a5a0a9671f97b2a4f35f231dc4e8596a168a61
Anazsa Blanding <anazsa@Anazsas-MacBook-Pro.local>
2022-07-14 10:06:58 -0400

    commit

--- src/main/java/com/codedifferently/problem01/Solution.java
+++ src/main/java/com/codedifferently/problem01/Solution.java
@@ -1,5 +1,8 @@
 package com.codedifferently.problem01;
 
+import javax.swing.*;
+import java.util.*;
+
 public class Solution {
     /**
      * You will be given an array of numbers, search the array and return the longest
@@ -7,10 +10,11 @@ public class Solution {
      * example input - {1,3,2,10,6,5,7,8}
      * return the String "Longest Set: {5 6 7 8}"
      * Even though 1,2,3 would the first consecutive set 5,6,7,8 is longer
+     *
      * @param allNumbers
      * @return
      */
-    public String findLongestConsecutiveSet(Integer[] allNumbers){
-        return null;
-    }
+    public String findLongestConsecutiveSet(Integer[] allNumbers) {
+       return null;
+}
 }
--- src/main/java/com/codedifferently/problem02/Solution.java
+++ src/main/java/com/codedifferently/problem02/Solution.java
@@ -1,7 +1,30 @@
 package com.codedifferently.problem02;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
-    public String howManyLettersDoYouSee(String input){
-        return null;
-    }
+    public String howManyLettersDoYouSee(String input) {
+
+        int[] count = new int[256];
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            count[ch] +=1;
+        }
+        int maxcount = -1 ;
+        char longest = 0 ;
+        for( char ch : input.toCharArray()) {
+            if(count[ch] > maxcount) {
+                maxcount = count[ch];
+                longest = ch ;
+            }
+
+        }
+        return String.valueOf(longest)+ ":"+ String.valueOf(maxcount);
+
+
+
+
+    }//main
 }
